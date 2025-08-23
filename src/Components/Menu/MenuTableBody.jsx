1865849c50dc19679e68b079de610b20a560b06e
Ssibu <sibu25456@gmail.com>
2025-08-23 11:53:49 +0530

    .

--- src/Components/Menu/MenuTableBody.jsx
+++ src/Components/Menu/MenuTableBody.jsx
@@ -1,3 +1,4 @@
+
 // components/MenuTable/MenuTableBody.jsx
 import React from "react";
 import SkeletonRow from "./SkeletonRow";
@@ -5,39 +6,44 @@ import TableRow from "./TableRow";
 
 const MenuTableBody = ({
   loading,
+  columns,
   data,
   entriesPerPage,
   currentPage,
   highlightMatch,
   searchTerm,
-  
 }) => {
   return (
     <table className="min-w-full border text-sm">
       <thead>
         <tr className="bg-gray-100 text-left">
-          <th className="p-2">SL.No</th>
-          <th className="p-2">Menu (In English)</th>
-          <th className="p-2">Menu (In Odia)</th>
-          <th className="p-2">Image</th>
-          <th className="p-2">Status</th>
-          <th className="p-2">Action</th>
+          {columns.map((column, index) => (
+            <th key={index} className="p-2">
+              {column.header}
+            </th>
+          ))}
         </tr>
       </thead>
       <tbody>
         {loading
           ? Array.from({ length: entriesPerPage }).map((_, idx) => (
-              <SkeletonRow key={idx} />
+              <SkeletonRow key={idx} columns={columns} />
             ))
           : data.map((item, idx) => (
               <TableRow
                 key={item.id}
                 item={item}
+                columns={columns}
                 idx={idx}
                 currentPage={currentPage}
                 entriesPerPage={entriesPerPage}
                 highlightMatch={highlightMatch}
                 searchTerm={searchTerm}
+                 pageContext={{
+                    currentPage,
+                    entriesPerPage,
+                    index: idx,
+                }}
               />
             ))}
       </tbody>
