845eea22223b66093aa7d8b82ea711c8f4e5932c
Ssibu <sibu25456@gmail.com>
2025-09-15 11:51:05 +0530

    .

--- src/Components/Admin/Spinner.jsx
+++
@@ -1,7 +0,0 @@
-export default function Spinner() {
-  return (
-    <div className="flex justify-center">
-      <div className="animate-spin rounded-full h-8 w-8 border-2 border-blue-500 border-t-transparent"></div>
-    </div>
-  );
-}
